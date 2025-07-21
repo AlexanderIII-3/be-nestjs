31e923dfd02cfbef57d395c2a112c9b388bf8547
AlexanderIII <oniichann183@gmail.com>
2025-07-21 21:44:01 +0700

    config email

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { PermissionsModule } from './permissions/permissions.module';
 import { RolesModule } from './roles/roles.module';
 import { DatabasesModule } from './databases/databases.module';
 import { SubscribersModule } from './subscribers/subscribers.module';
+import { MailModule } from './mail/mail.module';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
@@ -72,7 +73,9 @@ import { SubscribersModule } from './subscribers/subscribers.module';
 
     DatabasesModule,
 
-    SubscribersModule
+    SubscribersModule,
+
+    MailModule
 
   ],
 
